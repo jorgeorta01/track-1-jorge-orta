6f402cbbb84f8fd5ee740d74e36a3f2c1968cbfb
Jorge Orta <123342325+jorgeorta01@users.noreply.github.com>
2024-10-19 14:18:35 -0500

    added an accordian, got the bootstrap working

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
+app.use(express.static(path.join(__dirname)));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'main.html'));
 });
 
-
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.listen(port, () => { 
+    console.log(`Server running at http://localhost:${port}`); 
 });
